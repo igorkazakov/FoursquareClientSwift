e41b8b090b828516121303e5632d14699407c80e
IGOR_SENIOR <ikazakov@oggettoweb.com>
2018-09-25 17:33:57 +0300

    add to pods: alamofire, rxswift, Swinject, RxAlamofire

---
+++ FoursquareClientSwift/FoursquareClientSwiftTests/FoursquareClientSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  FoursquareClientSwiftTests.swift
+//  FoursquareClientSwiftTests
+//
+//  Created by ikazakov on 25.09.2018.
+//  Copyright © 2018 ikazakov. All rights reserved.
+//
+
+import XCTest
+@testable import FoursquareClientSwift
+
+class FoursquareClientSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
